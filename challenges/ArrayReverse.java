17746a3c3f2ed40584efa44638fa43418eabcec9
Jack Nelson <jnelson.java@gmail.com>
2020-09-14 14:07:27 -0700

    completed challenge with console printing for testing

--- challenges/ArrayReverse.java
+++ challenges/ArrayReverse.java
@@ -2,6 +2,25 @@
 
 public class ArrayReverse {
   public static void main(String[] args) {
-    System.out.println("testing java structure");
+    System.out.println("inside main");
+    reverseArray(new int[]{89, 2354, 3546, 23, 10, -923, 823, -12});
+  }
+
+  public static int[] reverseArray(int[] inputArr) {
+    System.out.println("inside reverseArray method");
+    int temp;
+    int len = inputArr.length;
+
+    for (int i = 0; i < len / 2; i++) {
+      temp = inputArr[i];
+      inputArr[i] =  inputArr[len - (1 + i)];
+      inputArr[len - (1 + i)] = temp;
+    }
+
+    for (int i = 0; i < len; i++) {
+      System.out.println(inputArr[i]);
+    }
+
+    return inputArr;
   }
 }
