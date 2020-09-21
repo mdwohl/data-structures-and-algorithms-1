f92a381070dfb29dfaa7dffc05044771ae0b0501
Jack Nelson <jnelson.java@gmail.com>
2020-09-21 15:26:45 -0700

    challenge 6 code completed

--- DataStructures/src/test/java/DataStructures/linkedList/LinkedListTest.java
+++ DataStructures/src/test/java/DataStructures/linkedList/LinkedListTest.java
@@ -32,4 +32,42 @@ public class LinkedListTest {
         assertFalse("Should return false", testList.includes(3));
     }
 
+    @Test public void testAppend() {
+        LinkedList testList = new LinkedList();
+        testList.append(2);
+        testList.insert(5);
+        testList.insert(5);
+        testList.insert(5);
+        System.out.println(testList);
+        assertEquals("should return string representing all nodes", "{5} -> {5} -> {5} -> {2} -> NULL", testList.toString());
+        testList.append(-4);
+        assertEquals("should now end with -4", "{5} -> {5} -> {5} -> {2} -> {-4} -> NULL", testList.toString());
+        System.out.println(testList);
+    }
+
+    @Test public void testInsertBefore() throws Exception {
+        LinkedList testList = new LinkedList();
+        testList.append(2);
+        testList.insert(5);
+        testList.insert(5);
+        testList.insert(5);
+        System.out.println(testList);
+        assertEquals("should return string representing all nodes", "{5} -> {5} -> {5} -> {2} -> NULL", testList.toString());
+        testList.insertBefore(2, 6);
+        assertEquals("should now start include 6 before 2", "{5} -> {5} -> {5} -> {6} -> {2} -> NULL", testList.toString());
+        System.out.println(testList);
+    }
+
+    @Test public void testInsertAfter() throws Exception {
+        LinkedList testList = new LinkedList();
+        testList.append(2);
+        testList.insert(5);
+        testList.insert(5);
+        testList.insert(5);
+        System.out.println(testList);
+        assertEquals("should return string representing all nodes", "{5} -> {5} -> {5} -> {2} -> NULL", testList.toString());
+        testList.insertAfter(5, 6);
+        assertEquals("should now start include 6 before 2", "{5} -> {6} -> {5} -> {5} -> {2} -> NULL", testList.toString());
+        System.out.println(testList);
+    }
 }
